58a018be9de158b4e72e49eda39e6088b9402fff
Ashikur Rahman <nillakash4097@gmail.com>
2024-02-04 10:22:10 +0600

    CalculatorMake

--- OstadAspClass2/OstadAspClass2/Calculator.cs
+++ OstadAspClass2/OstadAspClass2/Calculator.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace OstadAspClass2
 {
     public partial class Calculator : Form
     {
-        Double totalResult = 0;
-        String operatorValue = "";
+        string firstValue = "";
+        string secondValue = "";
+        string userInput = "";
+        double result = 0.0;
+        char operatorValue;
+        
 
         public Calculator()
         {
@@ -22,97 +19,135 @@ namespace OstadAspClass2
 
         private void oneButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(1);
+            outPutTextBox.Text += "";
+            userInput = "1";
+            outPutTextBox.Text += userInput;
         }
 
         private void twoButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(2);
+            outPutTextBox.Text += "";
+            userInput = "2";
+            outPutTextBox.Text += userInput;
         }
 
         private void threeButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(3);
+            outPutTextBox.Text += "";
+            userInput = "3";
+            outPutTextBox.Text += userInput;
         }
 
         private void fourButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(4);
+            outPutTextBox.Text += "";
+            userInput = "4";
+            outPutTextBox.Text += userInput;
         }
 
         private void fiveButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(5);
+            outPutTextBox.Text += "";
+            userInput = "5";
+            outPutTextBox.Text += userInput;
         }
 
         private void sixButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(6);
+            outPutTextBox.Text += "";
+            userInput = "6";
+            outPutTextBox.Text += userInput;
         }
 
         private void sevenButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(7);
+            outPutTextBox.Text += "";
+            userInput = "7";
+            outPutTextBox.Text += userInput;
         }
 
         private void eightButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(8);
+            outPutTextBox.Text += "";
+            userInput = "8";
+            outPutTextBox.Text += userInput;
         }
 
         private void nineButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(9);
+            outPutTextBox.Text += "";
+            userInput = "9";
+            outPutTextBox.Text += userInput;
         }
 
         private void zeroButton_Click(object sender, EventArgs e)
         {
-            KeyPressMethod(0);
-
-
+            outPutTextBox.Text += "";
+            userInput = "0";
+            outPutTextBox.Text += userInput;
         }
 
-        private void KeyPressMethod(int i)
-        {
-            if (outPutTextBox.Text == i.ToString())
-            {
-                outPutTextBox.Text = i.ToString();
-            }
-            else
-            {
-                outPutTextBox.Text += i.ToString();
-            }
-        }
+     
 
         private void plusButton_Click(object sender, EventArgs e)
         {
-            
+            operatorValue = '+';
+            firstValue = userInput;
+            userInput = "";
         }
 
         private void minusButton_Click(object sender, EventArgs e)
         {
-            
+            operatorValue = '-';
+            firstValue = userInput;
+            userInput = "";
         }
 
         private void intoButton_Click(object sender, EventArgs e)
         {
-            
+            operatorValue = '*';
+            firstValue = userInput;
+            userInput = "";
         }
 
         private void divButton_Click(object sender, EventArgs e)
         {
-           
+            operatorValue = '/';
+            firstValue = userInput;
+            userInput = "";
         }
 
         private void cancelButton_Click(object sender, EventArgs e)
         {
             outPutTextBox.Clear();
-            outPutTextBox.Text = "0";
+            
         }
 
         private void equalButton_Click(object sender, EventArgs e)
         {
-
+            secondValue = userInput;
+            double firtsNumber = double.Parse(firstValue);
+            double secondNumber =double.Parse(secondValue);
+            if (operatorValue == '+')
+            {
+                result = firtsNumber + secondNumber;
+                outPutTextBox.Text = result.ToString();
+            }
+            else if (operatorValue == '-')
+            {
+                result = firtsNumber - secondNumber;
+                outPutTextBox.Text = result.ToString();
+            }
+            else if (operatorValue == '*')
+            {
+                result = firtsNumber * secondNumber;
+                outPutTextBox.Text = result.ToString();
+            }
+            else if (operatorValue == '/')
+            {
+                result = firtsNumber / secondNumber;
+                outPutTextBox.Text = result.ToString();
+            }
         }
     }
 }
